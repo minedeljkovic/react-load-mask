21a887ac492cb2df646a0755026c06e8998dfaa0
Brad Christensen <brad.christensen@enlighten.co.nz>
2016-05-31 09:02:10 +1200

    Replace deprecated JSX compilers with Babel

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,10 +1,10 @@
 var gulp  = require('gulp');
-var react = require('gulp-react');
+var babel = require('gulp-babel');
 
 gulp.task('default', function () {
     return gulp.src('./src/**')
-        .pipe(react({
-            harmony: true
+        .pipe(babel({
+            presets: ['es2015', 'react']
         }))
         .pipe(gulp.dest('./lib'));
 });
